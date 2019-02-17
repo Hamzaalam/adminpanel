04fe6559b3a7ac90c9c86a164ff58fe92dd7a4ae
Hamzaalam <getright_02@live.com>
2019-02-17 08:51:38 +0500

    Initial commit

---
+++ blankpage.php
@@ -0,0 +1,18 @@
+<?php
+include('includes/header.php');
+include('includes/side-bar.php');
+
+?>
+
+ <!-- Page Content -->
+  <h1>Blank Page</h1>
+  <hr>
+  <p>This is a great starting point for new custom pages.</p>
+
+</div>
+<!-- /.container-fluid -->
+  
+
+<?php
+include('includes/footer.php');
+?>
