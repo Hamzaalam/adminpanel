29057ca08c45cc282b428af120f26ece373a2568
Hamzaalam <getright_02@live.com>
2019-02-17 16:43:20 +0500

    Candidate registration has been added

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -3,7 +3,7 @@
     <!-- Sidebar -->
     <ul class="sidebar navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="blankpage.php">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span>
         </a>
@@ -15,8 +15,9 @@
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
           <h6 class="dropdown-header">Login Screens:</h6>
-          <a class="dropdown-item" href="login.html">Login</a>
-          <a class="dropdown-item" href="register.html">Register</a>
+          <a class="dropdown-item" href="login.php">Login</a>
+          <a class="dropdown-item" href="register-voter.php">Register Voter</a>
+          <a class="dropdown-item" href="register-candidate.php">Register Candidate</a>
           <a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
           <div class="dropdown-divider"></div>
           <h6 class="dropdown-header">Other Pages:</h6>
@@ -25,17 +26,17 @@
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="charts.html">
+        <a class="nav-link" href="result.php">
           <i class="fas fa-fw fa-chart-area"></i>
           <span>Charts</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="register-candidate.php">
         <i class="far fa-address-card"></i>
           <span>Candidate Register</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="registered-candidate.php">
         <i class="far fa-address-card"></i>
           <span>Candidate List</span></a>
       </li>
---
+++ result.php
@@ -0,0 +1,24 @@
+<?php
+include('includes/header.php');
+include('includes/side-bar.php');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Result</title>
+	<script src="vendor/jquery/barchart.js"></script>
+</head>
+<body>
+
+<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+<script type="text/javascript" src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+	
+</body>
+</html>
+
+
+<?php
+include('includes/footer.php');
+?>
